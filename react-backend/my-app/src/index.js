52d0ba1aaac8b9f7b7b6a8c8cfecc1602d6a9db9
Gregg M <gmcmillion215@gmail.com>
2017-09-07 10:36:58 -0700

    Initial setup of React w/ Express

---
+++ react-backend/my-app/src/index.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import CreateAccount from './components/create_account';
+import Boards from './components/boards';
+import Board from './components/board';
+
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/" component={CreateAccount} />
+            <Route path="/boards" component={Boards} />
+            <Route path="/board" component={Board} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
